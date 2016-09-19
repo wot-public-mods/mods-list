787ad6adb75fc718db7a264572e8d3189b2ab9c4
Andrey Andruschushun <p0lir0id@yandex.ru>
2016-09-19 05:54:27 +0300

    update API example and README.md

--- api.py
+++ api.py
@@ -1,140 +1,90 @@
 
+"""
+	method addMod
+		:param id: Uniq modification ID - required
+		:param name: Modification name - required
+		:param description: Modification hint (mouse over) - required
+		:param icon: Modification icon (path from res_mods/<game_vaersion>/) - required
+		:param enabled: Is modification enabled (can be clicked) - required
+		:param login: Show modification on Login Window - required
+		:param lobby: Show modification in Lobby - required
+		:param callback: Called on modification click - required
+	
+	method updateMod
+		:param id: Uniq modification ID - required
+		:param name: Modification name - not necessary
+		:param description: Modification hint (mouse over) - not necessary
+		:param icon: Modification icon (path from res_mods/<game_vaersion>/) - not necessary
+		:param enabled: Is modification enabled (can be clicked) - not necessary
+		:param login: Show modification on Login Window - not necessary
+		:param lobby: Show modification in Lobby - not necessary
+		:param callback: Called on modification click - not necessary
+		
+	method alertMod
+		:param id: Uniq modification ID - required
+	
+	method clearAlert
+		:param id: Uniq modification ID - required
+"""
+
+
+
 from gui.mods.modsListApi import g_modsListApi
 
-
-"""
-	Картинка подгружается с папки
-"""
-
-with open('/'.join(['some', 'path', 'to', 'your', 'icon.png']), 'rb') as fh:
-	test_1_icon = fh.read().encode("base64").replace('\n', '')
-
-def test_1_callback():
-	print 'test_1_clicked'
+def test1_callback():
+	print 'test1_callback'
 
 g_modsListApi.addMod(
-	id = "test_1", 
-	name = 'test_1_mod_name', 
-	description = 'test_1_popup_description', 
-	icon = test_1_icon, 
+	id = "test1", 
+	name = 'test1 mod name', 
+	description = 'test1 hint', 
+	icon = 'scripts/client/gui/mods/test1.png', 
 	enabled = True, 
 	login = True, 
 	lobby = True, 
-	callback = test_1_callback
+	callback = test1_callback
 )
 
 
 
-
-
-
-
-
 """
-	Картинка занесена в питон
-"""
-
-test_2_icon = "iVBORw0KGgoAAAANSUhEUgAAADIAAAAyCAYAAAAeP4ixAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAAadEVYdFNvZnR3YXJlAFBhaW50Lk\
-5FVCB2My41LjEwMPRyoQAABkhJREFUaEPtWElsHEUUjQ05hAsRYA4ICQmhIHHimgMigQQIIeISwi4gYgkkQYAQkRBCLBIHhDggBBECoQjEAQm4IMD7vvtgj9fxMt7NeDbP6pnxLM17napOdbuNZ+zxzFhySe3\
-yq1/1+7/+/9evmn01NTWafKqrq41HHd8Ncp0IBMt47t+Hxn63YZXItpQ5nc6Xenp6AlNTU69pmnaPw+F4srGx0VtXV3fUbn6hsU4EQLMT5oNh/O/ZbDaJ3tS6urr8cv7i4uKl5eXlRCqVmoTor6WlpfdB1GOn\
-L19sEFGF/LJXzdA0t9v9MScvLCxcEkNaR0eHYRz7+vp6TzqdnoMoSCOj0WgK+F9gz+jo6PNcPz09fR54BGTX8GjsgZ3z8/Pv1tbWFoKMQcQkxAtW8SK2SRA7tba2lhX4PcrV+bOzs28lk8lMIpHohLzKKmfv9\
-/uTwvgrLpfrHD5QXHjwMrz2mHX+FrB9jliM7+WfUCiUknJ1PkQXYVQcoRLHl78AfEg1DviOTCYToo7JyclX0V3HMIP3shin929R9Vn154ivErETMqz4ctlopCqX81dXV/+BOA2jfCJsosDfDg0NPU05PPAGcJ\
-w6GIZch39vI3mOcR66CqmPvao/F6xzkB6xCqH8ZhgX5ssQ84tWOTHWHgkGg4z7FKb1w4tOhFiGa9D+QPwfFTmne5fzuR6EHgDU58F7AanPqj9XrBJZJwyHwz/xRbKtrKxcVuXsMbwfRH5kzGMLfhH4eo/H8xn\
-ngxxz4N65ubl3BE7I9UxwjqEl4bHzUp9Vf65YJWISiliX7RvRa+Pj42flYs7v7e19HMP3IeHfhh59PXAV5zLHhJ6TAmeEvGJwcPApjsHjEXR3S33sVf25Yp0IwLocgXLu9cyLqMDMg3UJz7BB0k5DtESPcL3w\
-DI30d3d3B2ZmZi4SIzw9zBEUymOAOrl4PJ5paWl52Pr+fLFBRBXiq/MrsQW4e3EyPSHG6JWInM+KHovF0gibGETfw+g38cSAGf+D/f39p1tbW30kAezxer2fs+Jjg6gBjrGnfvX9W8T2dSRXDGMO0jiR8KwT6\
-LIJFkXuepwPr5zC0YU7GcmR1Dy3XvRLwCdUfVb9eWD7HMkXj4yMPIckTyAPhmDczzx+qPKBgYEzyKOYqBucN4JtN9Te3n6Ccqu+LeCN60ghMYyvaGtre6S5udmHY8sLwIdIYqP5+WKdg/SIVYiXVfJL8uCHiv\
-wK8O2q3Dq/lFglogtpPOL5HMMAcTwD7EIse1nkRMK6EP+/qadaqayU2OQRClkPYOwUEpbJuVFjrHv7+vpWVGVcXyqsEwHQc4T7O6o3dx7uLn/i7vApjw/Dw8PPAN85MTHxMkItCO90AGeZsKoy9qXCBhECGLe\
-fWyi+tsb9H/gAb3hyMnBlU1PTMdYB/M8W57iUb/ayHcbX6giNhnHzeDJjY2Phzs7Ok9bFkN2EmvEdWSBnbA+SJcLmHIGRP9BIeGWFFRsJz5vfBI1GFUanJzxPxFlUdaPCy/UlxOY60tDQ4PX5fF/C0Fk8tg1k\
-3Ohe5xnJoqzQxuWMdQ7SI1LIHwSQ4M/C2OM4ZnzicDhC6D8CPs3TLPLECxIPWZWVEqtE1gnhnQdh/AFU4+PIF784kh8GsTN280uJTR6hkFsqjHVwdyJmwgN/wXBiURRnJbff7/+Kcq7bSHkxsU4EwMgRGCx/c\
-LiBGLtXBMbr111LCwcCga+hoGDGbAcbRKQQdYT3Ck2eSrFruTDGC9UVDN/FsxdOumGM8Ro7jLtGuST8tTrCQRgXJBHl1w79xzrkiResj3AxE533CTSeAG7kPLneoryY2JwjMoxQM/7mTRChphdInsEoFznzIT\
-2Ca2oa+NH/UV5MbK4j4qbHLx0FqRAemTNOkohEInQFwy8bDod/KdDPndvGOgfpEQr5Yxnywy9+nPuAd2oYnEomk/3AB+GFNpDTQOhX4CqGm1RmVV5MrBIxCfmlRRjdigPkEyyC/LEN+DCOJmeZQyShKlPXFxu\
-rRHJaLIpk5UbyUmGdCMCO39l3GhtE7IS7DJvryCaTyxnnlyNljM11xCLcNVjnID2y2eRyxiqRgisvJjZ5xCrcTVgnArBXR8oI79WRcsN7daSssEFEfSAwHqusfOU12n++ouKV+ygyygAAAABJRU5ErkJggg=="
-
-def test_2_callback():
-	print 'test_2_clicked'
-
-g_modsListApi.addMod(
-	id = "test_2", 
-	name = 'test_2_mod_name', 
-	description = 'test_2_popup_description', 
-	icon = test_2_icon, 
-	enabled = True, 
-	login = True, 
-	lobby = True, 
-	callback = test_2_callback
-)
-
-
-
-
-
-
-
-
-
-"""
-	Изменение языка относительно клиента (RU кластер + Тестовый сервер "Russian" / остальные "English")
+	Change lang (RU cluster "Russian", else "English")
 """
 
 from constants import AUTH_REALM
-rulang = True if AUTH_REALM in ['RU', 'CT'] else False
+ru_realm = True if AUTH_REALM in ['EU', 'CT'] else False
 
-def test_3_callback():
-	print 'test_3_кнопка_нажата' if rulang else 'test_3_clicked'
+def test2_callback():
+	print 'test2 кнопка нажата' if ru_realm else 'test2 button clicked'
 
 g_modsListApi.addMod(
-	id = "test_3", 
-	name = 'test_3_название_мода' if rulang else 'test_3_mod_name', 
-	description = 'test_3_подсказка_при_наведении' if rulang else 'test_3_popup_description', 
-	icon = test_1_icon, 
+	id = "test2", 
+	name = 'test2 название мода' if ru_realm else 'test2 mod name', 
+	description = 'test2 подсказка' if ru_realm else 'test2 hint', 
 	enabled = True, 
 	login = False, 
 	lobby = True, 
-	callback = test_3_callback
+	callback = test2_callback
 )
 
 
 
-
-
-
-
 """
-	Обновление данных кнопки мода (на примере test_1)
-	Можно передавать как все так и отдельные параметры
+	Update mod_item data (test2 in the example)
+	Passing of all parameters is not necessary
 """
-
-def test_1_callback_new():
-	print 'test_4_кнопка_нажата' if rulang else 'test_4_clicked'
 
 g_modsListApi.updateMod(
-	id = "test_1", 
-	name = 'test_1_название_мода' if rulang else 'test_1_mod_name', 
-	description = 'test_1_подсказка_при_наведении' if rulang else 'test_1_popup_description',
-	icon = test_1_icon, 	
-	enabled = False, 
-	login = True, 
-	lobby = True, 
-	callback = test_1_callback_new
-)
-
-g_modsListApi.updateMod(
-	id = "test_1", 
-	enabled = True, 
+	id = 'test2', 
+	icon = 'scripts/client/gui/mods/test2.png', 
+	login = True
 )
 
 
 
-
-
-
-
-
 """
-	Управления состоянием мода (оранжевый тикет + мигание кнопки) (на примере test_1)
+	Managing mod_item state (orange ticket + button blinking) (test1 in the example)
 """
 
-g_modsListApi.alertMod("test_1")
-g_modsListApi.clearAlert("test_1")
-
-
+g_modsListApi.alertMod("test1")
+g_modsListApi.clearAlert("test1")
