f497e1709de6094c638d4b56e5afe1560eb1f3cb
Andrey Andruschushun <p0lir0id@yandex.ru>
2018-01-26 02:45:31 +0300

    9.22 build

--- python/gui/modsListApi/__init__.py
+++ python/gui/modsListApi/__init__.py
@@ -1,11 +1,11 @@
 ﻿
 __author__ = "Andruschyshyn Andrey"
-__copyright__ = "Copyright 2017, Wargaming"
+__copyright__ = "Copyright 2018, Wargaming"
 __credits__ = ["Andruschyshyn Andrey"]
 __license__ = "CC BY-NC-SA 4.0"
-__version__ = "1.0.3"
+__version__ = "1.0.9"
 __maintainer__ = "Andruschyshyn Andrey"
-__email__ = "p0lir0id@yandex.ru"
+__email__ = "prn.a_andruschyshyn@wargaming.net"
 __status__ = "Production"
 
 from gui.modsListApi.controller import *
