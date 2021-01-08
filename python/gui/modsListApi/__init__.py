ae3b4303b783605fe803ae483002b710f8fd577c
Andrey Andruschyshyn <p0lir0id@yandex.ru>
2021-01-09 00:35:38 +0200

    package info update

--- python/gui/modsListApi/__init__.py
+++ python/gui/modsListApi/__init__.py
@@ -29,7 +29,7 @@ ModsListApi
 """
 
 __author__ = "Andruschyshyn Andrey"
-__copyright__ = "Copyright 2020, poliroid"
+__copyright__ = "Copyright 2021, poliroid"
 __credits__ = ["Andruschyshyn Andrey"]
 __license__ = "CC BY-NC-SA 4.0"
 __version__ = "1.3.5"
